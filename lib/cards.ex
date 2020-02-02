f2f420c961fb0136fac5696839ef43b753281e7c
Shawn Yates <yates.shawnp@gmail.com>
2020-02-02 11:38:49 -0800

    Section 2: Pattern Matching

--- lib/cards.ex
+++ lib/cards.ex
@@ -19,7 +19,31 @@ defmodule Cards do
   end
 
   def deal(deck, hand_size) do
+    # returns tuple containing two enums 
+    # (like array, but indexes are meaningful)
+    # { [...my_hand], [...rest_of_deck] }
     Enum.split(deck, hand_size)
   end
 
+  def save(deck, filename) do
+    deck_as_binary = :erlang.term_to_binary(deck)
+    File.write(filename, deck_as_binary)
+  end
+
+  def load(filename) do
+    case File.read(filename) do
+      # destructure result of read, try to pattern match with :ok
+      {:ok, binary} -> :erlang.binary_to_term binary
+      # if pattern match with :ok fails, try to pattern match with :error
+      {:error, _reason} -> "That file does not exist."
+    end
+  end
+
+  def create_hand(hand_size) do
+    # when using the pipe operator, the result of the precending function call
+    # is automatically passed as the first arg of the next function call
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
